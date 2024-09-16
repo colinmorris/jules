75cee38eb96f87d61f494fc388feffe4ec927a1a
colinmorris <colin.morris2@gmail.com>
2024-09-16 15:20:02 -0400

    oops

--- utils.py
+++ utils.py
@@ -5,6 +5,6 @@ def sibpath(fname):
     """Resolve fname to an absolute path corresponding to the file in the
     same directory as this file which has the given fname.
     """
-    pwd = Path(__file__).parent.resolve()
+    pwd = pathlib.Path(__file__).parent.resolve()
     path = os.path.join(pwd, fname)
     return path
